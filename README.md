10acb5eafadd85fd78e03ef9181f88f3f189c347
Daniele Costa <54379940+costadaniele@users.noreply.github.com>
2024-01-28 22:23:20 -0300

    conceitos react

---
+++ README.md
@@ -0,0 +1,9 @@
+`npm create vite@latest`
+
+`npm install`
+
+`npm run dev`
+
+<p>Styled Components</p>
+
+`npm install --save styled-components`
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import {Container} from './styles';
+import { Button } from '../../components/Button';
+
+export function Details() {
+    return (
+        <Container>
+            <Button title="Acessar" loading/>
+            <Button title="Logar"/>
+            <h1>Oi</h1>
+        </Container>
+
+    )
+}
